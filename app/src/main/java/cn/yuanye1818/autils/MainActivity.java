6f78c58cda1efaf8cf4751e3728c92c051268fb1
cute-PC\cute <codingnet@codingtu.com>
2018-10-17 17:54:52 +0800

    初始化

---
+++ app/src/main/java/cn/yuanye1818/autils/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.yuanye1818.autils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.yuanye1818.autils.core.CoreActivity;
+
+public class MainActivity extends CoreActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
