d586062193906d0e2f9608d92d888263df9db8c9
codingnet <codingnet@codingtu.com>
2018-11-29 02:46:19 +0800

    clickview

--- autils_compiler_annotation/src/main/java/cn/yuanye1818/autils/compiler/annotation/onclick/ClickView.java
+++ autils_compiler_annotation/src/main/java/cn/yuanye1818/autils/compiler/annotation/onclick/ClickView.java
@@ -1,9 +1,7 @@
 package cn.yuanye1818.autils.compiler.annotation.onclick;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.SOURCE)
 public @interface ClickView {
